a9bbcb6239370124160ba9e6fd76ec98e14b2b65
FadleenS <fadleen@gmail.com>
2020-07-10 18:54:44 +0800

    completed sound and animation implementation for drumkit

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-var g_timesPageLoaded = 0;
-localStorage.setItem("pgCtr", g_timesPageLoaded);
-g_timesPageLoaded++;
-
-//handles only 1 button
-// document.querySelector("button").addEventListener("mousedown",
-// () => console.log(`Button got clicked.`));
-
-//handles all buttons
-let btnsNodeList = document.querySelectorAll(".drum");
-
-// Array.prototype.forEach.call(btnsNodeList, function () {
-//   document.addEventListener("mousedown", (btnsNodeList) => {
-//     console.log(`Button was clicked.`);
-//   });
-// });
-
 //folder path
 let audioFolder = "sounds/";
 
@@ -64,30 +47,37 @@ const playAudio = function(evt){
         case "w":
         case "KeyW":
             audioObjs.tom_1.play();
+            buttonAnimation(evt);
             break;
         case "a":
         case "KeyA":
             audioObjs.tom_2.play();
+            buttonAnimation(evt);
             break;
         case "s":
         case "KeyS":
             audioObjs.tom_3.play();
+            buttonAnimation(evt);
             break;
         case "d":
         case "KeyD":
             audioObjs.tom_4.play();
+            buttonAnimation(evt);
             break;
         case "j":
         case "KeyJ":
             audioObjs.snare.play();
+            buttonAnimation(evt);
             break;
         case "k":
         case "KeyK":
             audioObjs.crash.play();
+            buttonAnimation(evt);
             break;
         case "l":
         case "KeyL":
             audioObjs.kick_bass.play();
+            buttonAnimation(evt);
             break;
         default:
             console.log("Not a valid selection.");
@@ -95,17 +85,77 @@ const playAudio = function(evt){
 
     }
 
-//add event listener to each button
-for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", playAudio);
+//reset animation for mouse click
+const resetAnimationClick = function(e){
+    console.log(this);
+    let element = document.getElementsByClassName(e.target.innerHTML)[0];
+    element.classList.remove("pressed");
 }
 
-//add keydown event listener to webpage
-document.addEventListener("keydown", playAudio);
+//reset animation for key realeased
+const resetAnimationKeybrd = function(e){
+    console.log(this);
+    switch(e.code){
+        case "KeyW":
+        case "KeyA":
+        case "KeyS":
+        case "KeyD":
+        case "KeyJ":
+        case "KeyK":
+        case "KeyL":
+            let activeBtn = document.querySelector("." + e.key);
+            activeBtn.classList.remove("pressed");
+            break;
+        default:
+            console.log("Not the correct key released.");
+    }
 
+}
+
+//add event listener to each button
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("mousedown", playAudio);
+  document.querySelectorAll(".drum")[i].addEventListener("mouseup", resetAnimationClick);
+  
+}
+
+//respond to keyboard in the document
+document.addEventListener("keydown", playAudio);
+document.addEventListener("keyup", resetAnimationKeybrd);
 
 //this.style.color = "white";
 
+function buttonAnimation(e){
+    //if mouseEvent do smth
+    if(e.type === "mousedown"){
+        switch(e.target.innerHTML){
+        case "w":
+        case "a":
+        case "s":
+        case "d":
+        case "j":
+        case "k":
+        case "l":
+            //returns HTML collection but we want the first and only element
+            let element = document.getElementsByClassName(e.target.innerHTML)[0];
+            console.log(element.outerHTML);
+            element.classList.add("pressed");
+            break;
+        default:
+            console.log("Not a valid button pressed.");
+
+        }
+         
+    }
+    //if keybrd pressed, do smth
+    if(e.type === "keydown"){
+        let activeBtn = document.querySelector("." + e.key);
+        activeBtn.classList.add("pressed");
+        console.log(this);
+        
+    }
+
+}
 
 
 
